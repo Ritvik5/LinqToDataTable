50612f097d9b9ea3037c6d7ea87a84d7532e78e1
Ritvik5 <sharma.ritvik56@gmail.com>
2023-04-20 10:38:31 +0530

    Welcome Message

---
+++ ProductReviewManagementWithLinq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewManagementWithLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management");
+        }
+    }
+}
